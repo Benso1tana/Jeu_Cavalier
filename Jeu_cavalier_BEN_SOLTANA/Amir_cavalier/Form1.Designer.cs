b8197dbd644908cd6735885088f22097693ad4d0
Mohamed amine BEN SOLTANA <medaminebensoltana@gmail.com>
2021-05-11 03:21:00 +0200

    Add files via upload

---
+++ Jeu_cavalier_BEN_SOLTANA/Amir_cavalier/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿
+namespace Amir_cavalier
+{
+    partial class Jeu_de_Cavalier
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Damier = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Rejouer = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Damier.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(652, 384);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 70);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jouer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Jouer);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(619, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 70);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Régles de jeu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Regle_cavalier);
+            // 
+            // Damier
+            // 
+            this.Damier.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Damier.Controls.Add(this.pictureBox2);
+            this.Damier.Controls.Add(this.pictureBox1);
+            this.Damier.Location = new System.Drawing.Point(75, 61);
+            this.Damier.Name = "Damier";
+            this.Damier.Size = new System.Drawing.Size(517, 484);
+            this.Damier.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(-45, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(562, 455);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, -9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(517, 493);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Rejouer
+            // 
+            this.Rejouer.Location = new System.Drawing.Point(619, 448);
+            this.Rejouer.Name = "Rejouer";
+            this.Rejouer.Size = new System.Drawing.Size(175, 53);
+            this.Rejouer.TabIndex = 7;
+            this.Rejouer.Text = "Rejouez";
+            this.Rejouer.UseVisualStyleBackColor = true;
+            this.Rejouer.Click += new System.EventHandler(this.Rejouer_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-7, -146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.ShowItemToolTips = true;
+            this.menuStrip1.Size = new System.Drawing.Size(847, 25);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.Click += new System.EventHandler(this.Rejouer_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(110, 21);
+            this.toolStripMenuItem1.Text = "Nouvelle Partie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(240, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 29);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(652, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Bienvenu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(598, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Votre prénom ?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(649, 109);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 13;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(598, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 29);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "label6";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(619, 366);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(175, 56);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "A propos";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Apropos);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(598, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "label7";
+            // 
+            // Jeu_de_Cavalier
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 548);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Rejouer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Damier);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Name = "Jeu_de_Cavalier";
+            this.Text = "Jeu_de_Cavalier";
+            this.Damier.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel Damier;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Rejouer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Jeu_cavalier_BEN_SOLTANA/Amir_cavalier/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// ce projet C# est ralisé par Amir BEN SOLTANA
+//ING2 Instrumentation
+//Encadré par Monsieur Karim FOUGHALI
+namespace Amir_cavalier
+{
+    public partial class Jeu_de_Cavalier : Form
+    {
+        const int DameSize = 8;   //la taille du Damier
+        const int btnSize = 60;  //taille de bouttons du Damier
+        int ClickButton = 0;    //clicker sur un boutton du Damier
+        int btn;               //un boutton du Damier
+        int btn_xPos = 0;     //la positon du boutton suivant l'axe x
+        int btn_yPos = 0;    //la positon du boutton suivant l'axe y
+        Button[,] Button_dame = new Button[8, 8];
+        int[] btnPas = new int[8]; //Déplacement
+        int[] BtnClicked = new int[64];//64 boutons de la Grille
+        bool ENDgame = true;//fin de jeu
+       
+        public Jeu_de_Cavalier() //Initialiser le jeu 
+        {
+            InitializeComponent();
+            label7.Text = "";
+            label7.Visible = false;
+            menuStrip1.Visible = false;
+            label3.Text = "Jeu Cavalier";
+            label3.ForeColor = Color.Red;
+            label3.Visible = true;
+            label4.Visible = true;
+            label5.Visible = true;
+            textBox1.Visible = true;
+            Rejouer.Visible = false;
+            pictureBox2.Image = Image.FromFile("image\\cavalier2.png");
+            button1.Visible = true;
+            button2.Visible = true;
+            pictureBox1.Visible = false;
+            label1.Visible = false;
+            label6.Visible = false;
+            button6.Visible = false;
+        }
+        private void btn_clicked(object sender, EventArgs e) //clicker sur un bouton de la Grille
+        {
+            button2.Enabled = false;
+            Button btn = sender as Button;
+            this.btn = Convert.ToInt32(btn.Name);
+           BtnClicked[ClickButton] = this.btn;
+            ClickButton += 1;
+            disableButton();
+           // proposer les différents chemins nécessaires pour le déplacement de cavalier
+           btnPas[0] = this.btn - 21;
+            btnPas[1] = this.btn - 19;
+            btnPas[2] = this.btn - 12;
+            btnPas[3] = this.btn - 8;
+            btnPas[4] = this.btn + 8;
+            btnPas[5] = this.btn + 12;
+            btnPas[6] = this.btn + 19;
+            btnPas[7] = this.btn + 21;
+            coleurDame(DameSize);//changement de coleur du damier
+            Deplacement(BtnClicked);// deplacer le cavalier
+            btn.BackgroundImage = Image.FromFile("image\\cavalier.jpg");//afficher l'image du cavalier
+            ENDgame = true;//fin de jeu
+            foreach (int choixBTN in btnPas)
+            {
+                int valide = verification(BtnClicked, choixBTN);
+                if (valide == 0)
+                {
+                    // Active le bouton 
+                    foreach (Button buttonToEnable in Button_dame)
+                    {
+                        if (choixBTN == Convert.ToInt32(buttonToEnable.Name))
+                        {
+                            buttonToEnable.Enabled = true;
+                            buttonToEnable.BackColor = Color.Green;// afficher le chemin de déplacement de cavalier par une couleur verte
+                            ENDgame = false;
+                        }
+                    }
+                }
+            }
+            if (ENDgame)
+            {
+                Findujeu(ClickButton);
+            }
+        }
+        private int verification(int[] array, int value)// verification si le cavalier peut se deplacer encore
+        {
+            foreach (int chemin in array)
+            {
+                if (value == chemin)
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        private void Bouton_Grille(int x, int y, int name, int color) // creation des boutons de la Grille
+        {
+            Button btn = new Button();
+            btn.Size = new Size(btnSize, btnSize);
+            Damier.Controls.Add(btn);
+            btn.Location = new Point(x, y);
+            if (color == 0)
+            {
+                btn.BackColor = Color.White;
+            }
+            if (color == 1)
+            {
+                btn.BackColor = Color.Red;
+            }
+            btn.Name = Convert.ToString(name);
+            btn.Text = Convert.ToString(name);
+            btn.Click += new EventHandler(btn_clicked);
+            Damier.Controls.Add(btn);
+        }
+        public void AfficheDame(int Damesaize)//Afficher le damier
+        {
+            for (int i = 0; i < Damesaize; i++)
+            {
+                btn_xPos = 0;
+                for (int j = 0; j < Damesaize; j++)
+                {
+                    Button_dame[i, j] = new Button();
+                    ((Button)Button_dame[i, j]).Name = "" + (i + 1) + j; 
+                    ((Button)Button_dame[i, j]).Size = new Size(btnSize, btnSize);
+                    ((Button)Button_dame[i, j]).Location = new Point(btn_xPos, btn_yPos);
+                    ((Button)Button_dame[i, j]).Click += new EventHandler(btn_clicked);
+                    Damier.Controls.Add((Button)Button_dame[i, j]);
+                    btn_xPos += btnSize;
+                }
+                    btn_yPos += btnSize;
+            }
+        }
+        public void coleurDame(int Damesize)//Initialiser le damier avec des couleurs (blanc et marron)
+        {
+            for (int i = 0; i < Damesize; i++)
+            {
+                for (int j = 0; j < Damesize; j++)
+                {
+                    ((Button)Button_dame[i, j]).BackgroundImage = null;
+                    if (j % 2 == 0 && i % 2 == 0 || j % 2 == 1 && i % 2 == 1)
+                    {
+                        ((Button)Button_dame[i, j]).BackColor = Color.White;
+                    }
+                    else
+                    {
+                        ((Button)Button_dame[i, j]).BackColor = Color.Black;
+                    }
+                }
+            }
+        }
+        private void Deplacement(int[] buttonUsed) // deplacer le cavalier 
+        {
+            foreach (Button colorButton in Button_dame)
+            {
+                for (int i = 0; i < ClickButton; i++)
+                {
+                    if (buttonUsed[i] == Convert.ToInt32(colorButton.Name))
+                    {
+                        colorButton.BackgroundImage= Image.FromFile("image\\tic.jpg");
+                    }
+                }
+            }
+        }
+        private void Jouer(object sender, EventArgs e) // lancer le jeu
+        {
+            label6.Visible = true;
+            label6.Text = "Placé votre Cavalier \n vous avez le choix !";
+            menuStrip1.Visible = true;
+            label3.Visible = false;
+            pictureBox2.Visible = false;
+            label7.Visible = true;
+            label4.Visible = false;
+            label5.Visible = false;
+            textBox1.Visible = false;
+            AfficheDame(DameSize);
+            coleurDame(DameSize);
+            button1.Visible = false;
+            button2.Visible = false;
+            button6.Visible = true;
+        }
+        private void Findujeu(int nbButtonClicked) // fin du jeu et affichage de score obtenu
+        {
+            label5.Text= textBox1.Text;
+            disableButton();
+            if (nbButtonClicked == 64)// si on termine le jeu 
+            {
+               
+                label7.Text = label5.Text+"!  Vous avez gagné !";
+                Rejouer.Visible = true;
+            }
+            else
+            { 
+                label7.Text = label5.Text+"! Dommage Vous avez perdu \n  vous avez obtenu " + nbButtonClicked + " points";
+                pictureBox1.Image = Image.FromFile("image\\game over.jpg");
+                pictureBox1.Visible = true;
+                Rejouer.Visible = true;
+            }
+        }
+        private void disableButton()
+        {
+            foreach (Button buttonDisabled in Button_dame)  //Verrouillage de tous les boutons
+            {
+                buttonDisabled.Enabled = false;
+            }
+        }
+        private void Regle_cavalier(object sender, EventArgs e)// Règles de jeu
+        {
+          MessageBox.Show("Cliquez sur une case, puis déplacez \nvotre cavalier selon les règles du jeu d'échec.\n\nLe but est d'atteindre un maximum \nde cases.", "Tableau des scores", MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button2);
+        }
+        private void Rejouer_Click(object sender, EventArgs e) // Rejouer une partie
+        {
+           
+            ClickButton = 0;
+            coleurDame(DameSize);
+            foreach (Button buttonDisabled in Button_dame) //Verrouillage de tous les boutons
+            {
+                buttonDisabled.Enabled = true;
+                buttonDisabled.Text = "";
+            }
+            pictureBox1.Visible = false;
+            Rejouer.Visible = false;
+            button2.Enabled = true;
+            label1.Text = "";
+        }
+        private void Aleatoire(object sender, EventArgs e)
+        {
+            //button2.Visible = false;
+            //button3.Enabled = false;
+            Array.Clear(BtnClicked, 0, BtnClicked.Length);
+            Array.Clear(btnPas, 0, btnPas.Length);
+            Random alea= new Random();
+            int l = alea.Next(0, 7);     //un entier aleatoire compris entre 0 et 7
+            int x = alea.Next(0, 7);
+            btn = Convert.ToInt32(Button_dame[l,x].Name);
+            BtnClicked[ClickButton] = btn;
+            ClickButton += 1;
+            Button_dame[l, x].Enabled = false;
+            //proposer les différents chemins nécessaires pour le déplacement de cavalier
+            btnPas[0] = btn - 21;                      
+            btnPas[1] = btn - 19; 
+            btnPas[2] = btn - 12;  
+            btnPas[3] = btn - 8;   
+            btnPas[4] = btn + 8;  
+            btnPas[5] = btn + 12;  
+            btnPas[6] = btn + 19;  
+            btnPas[7] = btn + 21;  
+            ENDgame = true;
+
+            foreach (int choixBtn in btnPas)
+            {
+                int isValid = verification(BtnClicked, choixBtn);
+                if (isValid == 0)//le cavalier peut se déplacer encore 
+                {
+
+                    foreach (Button buttonToEnable in Button_dame) //activer le bouton et changer le couleur
+                    {
+                        if (choixBtn == Convert.ToInt32(buttonToEnable.Name))
+                        {
+                            buttonToEnable.Enabled = true;
+                            buttonToEnable.BackColor = Color.Red;
+                            ENDgame = false;
+                        }
+                    }
+                }
+                Button_dame[l, x].BackColor=Color.Yellow;
+
+            }
+        }
+
+        private void Apropos(object sender, EventArgs e)//Afficher un message pour décrire le jeu de cavalier
+        {
+            MessageBox.Show("Ce jeu a été réalisé dans le cadre d'un projet sous C# par Mohamed Amine BEN SOLTANA. \nLe but est d'atteindre un maximum de cases."," jeu du cavalier", MessageBoxButtons.OK, MessageBoxIcon.None);
+        }
+    }
+}
+
+
+
